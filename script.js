0a95398cfac13a6cfd0c53a70fdf959babf3cf30
cookielivecenter <152007098+cookielivecenter@users.noreply.github.com>
2023-11-25 14:27:37 +0100

    1.2 script.js

--- script.js
+++ script.js
@@ -52,6 +52,44 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function unlockSecretPage() {
+    alert('DeepWeb Unlock');
+    
+    // Crear un nuevo elemento iframe para cargar el HTML dentro de la misma página
+    const iframe = document.createElement('iframe');
+    iframe.src = 'secret.html'; // Cambia 'ruta_del_html_secreto.html' por la ruta de tu HTML secreto
+    iframe.width = '100%';
+    iframe.height = '500px';
+    
+    // Limpia el contenido actual de la página y agrega el iframe
+    document.body.innerHTML = '';
+    document.body.appendChild(iframe);
+  }
+});
+
+      activateUltraMaster();
+    }
+
+    // Verificar si se ha alcanzado la cantidad de clics para mostrar el mensaje de desbloqueo
+    if (score >= 1500) {
+      unlockSecretPage();
+    }
+  });
+
+  function activateUltraClicks() {
+    ultraClicksActive = true;
+    scoreValue.style.color = 'gold'; // Cambia el color según tus preferencias
+    multiplierMessage.textContent = '¡Multiplicador x2 activado!';
+    alert('¡ULTRACLICKS activado! ¡Ahora obtienes 2 puntos por clic!');
+  }
+
+  function activateUltraMaster() {
+    ultraMasterActive = true;
+    scoreValue.style.color = 'purple'; // Cambia el color según tus preferencias
+    multiplierMessage.textContent = '¡Multiplicador x3 activado!';
+    alert('¡ULTRAMASTER activado! ¡Ahora obtienes 3 puntos por clic!');
+  }
+
+  function unlockSecretPage() {
     alert('¡Has desbloqueado la página web secreta!');
     // Cambia 'https://tupagina.secreta' por el enlace de tu página secreta
     window.location.href = 'https://tupagina.secreta';
